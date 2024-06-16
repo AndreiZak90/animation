c156f6630c12aa593474a789b5b18489993bf8b9
AppVeyor <ci@appveyor.com>
2024-06-16 14:08:33 +0000

    45b86f9

---
+++ 837b6728056019eb45ac.js
@@ -0,0 +1,1 @@
+export default class Collapse{init(){const e=document.querySelector(".collapse_btn"),l=document.querySelector(".collapse_box");e.addEventListener("click",(()=>{l.classList.toggle("collapse_box_max")}))}}
